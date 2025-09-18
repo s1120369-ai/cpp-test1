d6ed0a9b24012a5485260b0390ab835a3910e210
s1120369-ai <s1120369@dmhs.kh.edu.tw>
2025-09-18 07:00:33 +0000

    hw2

--- test1/main.cpp
+++ test1/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+using namespace std;
+// 華視攝氏轉換
+
 int main() {
-    std::cout <<"Hello world";
+    double fahrenheit;
+    double celsius;
+    cout << "請輸入華氏溫度:\n";
+    cin >> fahrenheit;
+    celsius = (fahrenheit - 32) / 9 * 5;
+    cout << "攝氏溫度是:" << celsius;
     return 0;
 }
