1684e21bdb3e20b96e8de2719f8332a1032a23d3
s1120369-ai <s1120369@dmhs.kh.edu.tw>
2025-10-16 14:38:02 +0800

    hw5

--- test1/main.cpp
+++ test1/main.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 int main() {
-    int sum = 0;
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0)
-            cout << i << " ";
+    // 第一部分：印出星號三角形
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
+        }
+        cout << endl;
     }
+
+    cout << endl; // 分隔兩部分
+
+    // 第二部分：印出九九乘法表
+    int i = 1;
+    while (i <= 9) {
+        int j = 1;
+        while (j <= 9) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
     return 0;
 }
