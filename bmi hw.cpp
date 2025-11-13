c9c4b68f954924515e1e25d72756daa35a5fc24f
s1120369-ai <s1120369@dmhs.kh.edu.tw>
2025-11-13 14:49:16 +0800

    Add files via upload

---
+++ bmi hw.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+double calculateBMI(double weight_kg, double height_cm) {
+    if (height_cm <= 0) return 0.0;
+
+    double height_m = height_cm / 100.0; // Convert height to meters
+    double bmi = weight_kg / (height_m * height_m); // BMI calculation
+    return bmi;
+}
+
+int main() {
+    double weight1, weight2, height1, height2;
+
+    weight1 = 80;
+    weight2 = 70;
+    height1 = 168;
+    height2 = 188;
+
+    std::cout << "Your bmi is " << calculateBMI(weight1, height1) << std::endl;
+    std::cout << "Another bmi is " << calculateBMI(weight2, height2) << std::endl;
+
+    return 0;
+}
